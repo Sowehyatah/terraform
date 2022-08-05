01c24d1b17d4a965df32aadace64b7d30838e4ae
Sowehyatah <98928190+Sowehyatah@users.noreply.github.com>
2022-08-04 20:12:27 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "instance_ip_addr" {
-  value = aws_instance.ec2_demo.private_ip
+  value = aws_instance.my-ec2.private_ip
 }
 
 output "instance_ip_public" {
-  value = aws_instance.ec2_demo.public_ip
+  value = aws_instance.my-ec2.public_ip
 }
